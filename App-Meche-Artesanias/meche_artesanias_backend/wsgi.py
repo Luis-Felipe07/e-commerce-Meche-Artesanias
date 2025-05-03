eb25a07f059a4b174ea0cbba932d00a615343687
jhan07-sanchez <jhansancheza@gmail.com>
2025-05-02 20:16:50 -0500

    realice cambio en wsgi.py

--- App-Meche-Artesanias/meche_artesanias_backend/wsgi.py
+++ App-Meche-Artesanias/meche_artesanias_backend/wsgi.py
@@ -1,3 +1,18 @@
+import sys
+import os
+
+# Agregar el directorio raíz de tu proyecto al PYTHONPATH
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from django.core.wsgi import get_wsgi_application
+
+# Establecer el módulo de configuración de Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meche_artesanias_backend.settings')
+
+application = get_wsgi_application()
+
+
+
 """
 WSGI config for meche_artesanias_backend project.
 
@@ -7,10 +22,5 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import os
 
-from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meche_artesanias_backend.settings')
-
-application = get_wsgi_application()
